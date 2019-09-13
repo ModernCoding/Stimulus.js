e9f743b67696b2cf624382595659cd730e44857a
Cormorant Programming <cormorant.programming@gmail.com>
2019-09-13 19:00:12 +0700

    clipboard_controller.js

---
+++ src/controllers/clipboard_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "source" ]
+
+  connect() {
+    if (document.queryCommandSupported("copy")) {
+      this.element.classList.add("clipboard--supported")
+    }
+  }
+
+  copy(event) {
+    event.preventDefault()
+    this.sourceTarget.select()
+    document.execCommand("copy")
+  }
+}
